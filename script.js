da7cce359edf3258bcd6015a4d068e0c39ed2b75
kotesh-naik <160763127+kotesh-naik@users.noreply.github.com>
2024-06-27 10:52:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+ getSumBtn.disabled = true;
+	const prices = document.querySelectorAll(".price");
+	let sum = 0;
+	prices.forEach((price)=>{
+		const value = parseInt(price.textContent);
+		if(! isNoN(value)){
+			sum += value;
+		}
+	})
+	const totalPriceRow = document.createElement("tr");
+	totalPriceRow.id = "ans";
+	const totalPriceData = document.createElement("td");
+	const totalPriceAns= document.createElement("td");
+	totalPriceRow.appendChild(totalPriceData);
+	totalPriceRow.appendChild(totalPriceAns);
+	const data = ' total price (in Rs) : ';
+	totalPrice.Data.append('total price( in Rs):');
+	const table = document.querySelector("tbody");
+	table.appendChild(totalPriceRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
